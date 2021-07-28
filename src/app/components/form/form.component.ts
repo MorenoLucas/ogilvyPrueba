9f88efe6e681a5f51327131d144deaca4b12fb34
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-07-28 18:55:16 +0200

    agregamos modulos

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  formulario!: FormGroup;
+  nombre: any = '';
 
-  constructor() { }
+  //mostrar mensaje de error
+  // <p *ngIf="!contro.valid Y control.touched  Y control.errors?.required">No es valido</p>
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nombre: ['', Validators.required],
+    });
+    console.log(this.formulario.getRawValue);
   }
-
 }
